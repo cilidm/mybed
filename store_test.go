2c5393a83020851be4978065fd53ac691123a42e
hahax <75506525@qq.com>
2020-08-13 22:02:20 +0800

    加入运行判断

--- store_test.go
+++ store_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"mybedv2/app/system/model/store"
 	storeService "mybedv2/app/system/service/store"
 	"strings"
@@ -12,9 +11,9 @@ func TestStore(t *testing.T) {
 	storeConf := store.GetStoreConfig("cloud_type", "cs-minio")
 	cs, _ := storeService.NewCloudStoreByConf(storeConf, false)
 	storePath := strings.ReplaceAll("http://127.0.0.1:9000/mybed/mybed/1/1032594_1.jpg", storeConf.PublicBucketDomain+storeConf.PublicBucket, "")
-	fmt.Println(storePath)
-	fmt.Println(cs.IsExist(storePath))
+	t.Log(storePath)
+	t.Log(cs.IsExist(storePath))
 	if err := cs.Delete(storePath); err != nil {
-		fmt.Println(err)
+		t.Error(err)
 	}
 }
