d270cf3be8fc1dab2ce924ed045cc3926abfc1b0
hahax <75506525@qq.com>
2020-08-13 21:45:42 +0800

    readme

--- store_test.go
+++ store_test.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"mybedv2/app/system/model/code"
 	"mybedv2/app/system/model/store"
 	storeService "mybedv2/app/system/service/store"
 	"strings"
-	"time"
+	"testing"
 )
 
-func testStore() {
+func TestStore(t *testing.T) {
 	storeConf := store.GetStoreConfig("cloud_type", "cs-minio")
 	cs, _ := storeService.NewCloudStoreByConf(storeConf, false)
 	storePath := strings.ReplaceAll("http://127.0.0.1:9000/mybed/mybed/1/1032594_1.jpg", storeConf.PublicBucketDomain+storeConf.PublicBucket, "")
@@ -18,19 +17,4 @@ func testStore() {
 	if err := cs.Delete(storePath); err != nil {
 		fmt.Println(err)
 	}
-}
-
-func testMany2Many() {
-	var codeEntity code.Entity
-	codeEntity.Insert(code.BindForm{
-		Code:      "aaa",
-		Value:     100,
-		Status:    1,
-		CreatedAt: time.Now(),
-		UserId:    1,
-	})
-}
-
-func main() {
-	//testMany2Many()
 }
