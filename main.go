19e5cf03e8bd7e76c2d1ad204876d3b8ffe1ce31
haha <75506525@qq.com>
2020-08-24 20:12:56 +0800

    修复一些bug

--- main.go
+++ main.go
@@ -20,30 +20,4 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
-
-	//sysType := runtime.GOOS
-	//if sysType == "windows" {
-	//	r := router.InitRouter()
-	//	s := &http.Server{
-	//		Addr:           fmt.Sprintf(":%d", conf.Setting.HTTPPort),
-	//		Handler:        r,
-	//		ReadTimeout:    conf.Setting.ReadTimeout,
-	//		WriteTimeout:   conf.Setting.WriteTimeout,
-	//		MaxHeaderBytes: 1 << 20,
-	//	}
-	//	s.ListenAndServe()
-	//} else {
-	//	endless.DefaultReadTimeOut = conf.Setting.ReadTimeout
-	//	endless.DefaultWriteTimeOut = conf.Setting.WriteTimeout
-	//	endless.DefaultMaxHeaderBytes = 1 << 20
-	//	endPoint := fmt.Sprintf(":%d", conf.Setting.HTTPPort)
-	//	server := endless.NewServer(endPoint, router.InitRouter())
-	//	server.BeforeBegin = func(add string) {
-	//		log.Printf("Actual pid is %d And port is %d", syscall.Getpid(), conf.Setting.HTTPPort)
-	//	}
-	//	err := server.ListenAndServe()
-	//	if err != nil {
-	//		log.Printf("Server err: %v", err)
-	//	}
-	//}
 }
